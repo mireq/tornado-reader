52ff2005fffbe47b8bf61362bad7a2ce628972ef
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-02-06 12:29:15 +0100

    Added js utils

--- tornado_reader/static/js/login.js
+++ tornado_reader/static/js/login.js
@@ -1,5 +1,9 @@
-(function(){
+(function(_){
+
+var contentElement = _.id('content');
+var loginForm = _.elemAppend(contentElement, 'FORM', {'class': 'login'});
+var signupForm = _.elemAppend(contentElement, 'FORM', {'clas': 'signup'});
 
 
 
-})();
+})(_utils);
--- tornado_reader/static/js/utils.js
+++ tornado_reader/static/js/utils.js
@@ -4,6 +4,57 @@
 
 window._utils = {};
 
+// DOM
+var el = document.createElement('DIV');
 
+var elem = function(elementName, attrs, content) {
+	var element = document.createElement(elementName);
+	if (attrs !== undefined) {
+		for (var attrName in attrs) {
+			if (has(attrs, attrName)) {
+				element.setAttribute(attrName, attrs[attrName]);
+			}
+		}
+	}
+
+	if (content !== undefined) {
+		element.appendChild(document.createTextNode(content));
+	}
+	return element;
+};
+
+var elemAppend = function(parent, elementName, attrs, content) {
+	var el = elem(elementName, attrs, content);
+	parent.appendChild(el);
+	return el;
+};
+
+var byId = function(elementId) {
+	return document.getElementById(elementId);
+};
+
+var byCls;
+if (el.getElementsByClassName === undefined) {
+	byCls = function(parent, cls) {
+		var elements = parent.getElementsByTagName('*');
+		var match = [];
+		for (var i = 0, leni = elements.length; i < leni; i++) {
+			if (hasClass(elements[i], cls)) {
+				match.push(elements[i]);
+			}
+		}
+		return match;
+	};
+}
+else {
+	byCls = function(parent, cls) {
+		return parent.getElementsByClassName(cls);
+	};
+}
+
+_utils.elem = elem;
+_utils.elemAppend = elemAppend;
+_utils.id = byId;
+_utils.cls = byCls;
 
 })();
